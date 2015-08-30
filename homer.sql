1f1538e12b9ef7242ee26b35c37ebddda0ec1666
Travis Cross <tc@traviscross.com>
2015-08-30 10:47:51 +0000

    Create postgresql version of other HOMER tables
    Here we transcribe the canonical mysql version of the tables as found
    in the HOMER repository to postgresql.

--- homer.sql
+++ homer.sql
@@ -51,3 +51,179 @@ CREATE INDEX sip_capture_auth_user_idx ON sip_capture (auth_user);
 CREATE INDEX sip_capture_callid_aleg_idx ON sip_capture (callid_aleg);
 CREATE INDEX sip_capture_date_idx ON sip_capture (date);
 CREATE INDEX sip_capture_callid_idx ON sip_capture (callid);
+
+-- homer_users.sql
+
+CREATE TABLE homer_hosts (
+  id SERIAL NOT NULL,
+  host VARCHAR(80) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  status SMALLINT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (host)
+);
+
+CREATE TABLE homer_logon (
+  userid SERIAL NOT NULL,
+  useremail VARCHAR(50) NOT NULL DEFAULT '',
+  password VARCHAR(50) NOT NULL DEFAULT '',
+  userlevel INTEGER NOT NULL DEFAULT '0',
+  PRIMARY KEY (userid)
+);
+
+CREATE TABLE homer_nodes (
+  id SERIAL NOT NULL,
+  host VARCHAR(80) NOT NULL,
+  dbname VARCHAR(100) NOT NULL,
+  dbport VARCHAR(100) NOT NULL,
+  dbusername VARCHAR(100) NOT NULL,
+  dbpassword VARCHAR(100) NOT NULL,
+  dbtables VARCHAR(100) NOT NULL DEFAULT 'sip_capture',
+  name VARCHAR(100) NOT NULL,
+  status SMALLINT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (host)
+);
+
+CREATE TABLE homer_searchlog (
+  id SERIAL NOT NULL,
+  useremail VARCHAR(50) NOT NULL,
+  date TIMESTAMP NOT NULL,
+  search TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE INDEX ON homer_searchlog (useremail);
+CREATE INDEX ON homer_searchlog (date);
+
+-- statistics.sql
+
+CREATE TABLE alarm_data (
+  id BIGSERIAL NOT NULL,
+  create_date TIMESTAMP NOT NULL,
+  type VARCHAR(50) NOT NULL DEFAULT '',
+  total INTEGER NOT NULL,
+  source_ip VARCHAR(150) NOT NULL DEFAULT '0.0.0.0',
+  description VARCHAR(256) NOT NULL,
+  status SMALLINT NOT NULL DEFAULT '1',
+  PRIMARY KEY (id,create_date)
+);
+CREATE INDEX ON alarm_data (create_date);
+CREATE INDEX ON alarm_data (type);
+
+CREATE TABLE alarm_data_mem (
+  id BIGSERIAL NOT NULL,
+  create_date TIMESTAMP NOT NULL,
+  type VARCHAR(50) NOT NULL DEFAULT '',
+  total INTEGER NOT NULL,
+  source_ip VARCHAR(150) NOT NULL DEFAULT '0.0.0.0',
+  description VARCHAR(256) NOT NULL,
+  status SMALLINT NOT NULL DEFAULT '1',
+  PRIMARY KEY (id),
+  UNIQUE (type,source_ip)
+);
+CREATE INDEX ON alarm_data (create_date);
+CREATE INDEX ON alarm_data (type);
+
+CREATE stats_data (
+  id BIGSERIAL NOT NULL,
+  from_date TIMESTAMP NOT NULL,
+  to_date TIMESTAMP NOT NULL,
+  type VARCHAR(50) NOT NULL DEFAULT '',
+  total INTEGER NOT NULL,
+  PRIMARY KEY (id,from_date),
+  UNIQUE (from_date,to_date,type)
+);
+CREATE INDEX ON stats_data (from_date);
+CREATE INDEX ON stats_data (to_date);
+CREATE INDEX ON stats_data (type);
+
+CREATE TABLE stats_ip (
+  id BIGSERIAL NOT NULL,
+  from_date TIMESTAMP NOT NULL,
+  to_date TIMESTAMP NOT NULL,
+  method VARCHAR(50) NOT NULL DEFAULT '',
+  source_ip VARCHAR(255) NOT NULL DEFAULT '0.0.0.0',
+  total INTEGER NOT NULL,
+  PRIMARY KEY (id,from_date),
+  UNIQUE (from_date,to_date,method,source_ip)
+);
+CREATE INDEX ON stats_ip (from_date);
+CREATE INDEX ON stats_ip (to_date);
+CREATE INDEX ON stats_ip (method);
+
+CREATE TABLE stats_ip_mem (
+  id BIGSERIAL NOT NULL,
+  create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  method VARCHAR(50) NOT NULL DEFAULT '',
+  source_ip VARCHAR(255) NOT NULL DEFAULT '0.0.0.0',
+  total INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (method,source_ip)
+);
+
+CREATE TABLE stats_method (
+  id BIGSERIAL NOT NULL,
+  from_date TIMESTAMP NOT NULL,
+  to_date TIMESTAMP NOT NULL,
+  method VARCHAR(50) NOT NULL DEFAULT '',
+  auth SMALLINT NOT NULL DEFAULT '0',
+  cseq VARCHAR(100) NOT NULL,
+  totag SMALLINT NOT NULL,
+  total INTEGER NOT NULL,
+  PRIMARY KEY (id,from_date),
+  UNIQUE (from_date,to_date,method,auth,totag,cseq)
+);
+CREATE INDEX ON stats_method (from_date);
+CREATE INDEX ON stats_method (to_date);
+CREATE INDEX ON stats_method (method);
+CREATE INDEX ON stats_method (cseq);
+
+CREATE TABLE stats_method_mem (
+  id BIGSERIAL NOT NULL,
+  create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  method VARCHAR(50) NOT NULL DEFAULT '',
+  auth SMALLINT NOT NULL DEFAULT '0',
+  cseq VARCHAR(100) NOT NULL,
+  totag SMALLINT NOT NULL,
+  total INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (method,auth,totag, cseq)
+);
+CREATE INDEX ON stats_method (create_date);
+CREATE INDEX ON stats_method (method);
+CREATE INDEX ON stats_method (cseq);
+
+CREATE TABLE stats_useragent (
+  id BIGSERIAL NOT NULL,
+  from_date TIMESTAMP NOT NULL,
+  to_date TIMESTAMP NOT NULL,
+  useragent VARCHAR(100) NOT NULL DEFAULT '',
+  method VARCHAR(50) NOT NULL DEFAULT '',
+  total INTEGER NOT NULL DEFAULT '0',
+  PRIMARY KEY (id,from_date),
+  UNIQUE (from_date,to_date,method,useragent)
+);
+CREATE INDEX ON stats_useragent (from_date);
+CREATE INDEX ON stats_useragent (to_date);
+CREATE INDEX ON stats_useragent (useragent);
+CREATE INDEX ON stats_useragent (method);
+CREATE INDEX ON stats_useragent (total);
+
+CREATE TABLE stats_useragent_mem (
+  id BIGSERIAL NOT NULL,
+  create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  useragent VARCHAR(100) NOT NULL DEFAULT '',
+  method VARCHAR(50) NOT NULL DEFAULT '',
+  total INTEGER NOT NULL DEFAULT '0',
+  PRIMARY KEY (id),
+  UNIQUE (useragent,method)
+);
+
+CREATE TABLE alarm_config (
+  id SERIAL NOT NULL,
+  create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  type VARCHAR(50) NOT NULL,
+  value INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (type)
+);
